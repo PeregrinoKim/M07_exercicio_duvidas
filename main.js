92a20e42ca034497ec96d3259b1b034c221992b7
Joaquim Araújo <joaquim.araujox@gmail.com>
2023-03-12 17:09:25 -0300

    Atualização e correção

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('validar');
-const valorA = parseFloat(document.getElementById('info-a').value);
-const valorB = parseFloat(document.getElementById('info-b').value);
+
 formEValido = false;
 
 function validaCampo (valorA, valorB){
@@ -8,6 +7,8 @@ function validaCampo (valorA, valorB){
 }
 form.addEventListener('submit', function(e){
     e.preventDefault();
+const valorA = parseFloat(document.getElementById('info-a').value);
+const valorB = parseFloat(document.getElementById('info-b').value);
     const MensagemSucesso = 'Sucesso';
     const MensagemErro = 'Incorreto Valor B Menor';
 
@@ -16,13 +17,15 @@ form.addEventListener('submit', function(e){
         const containerMensagemSucesso = document.querySelector('.message-sucess')
         containerMensagemSucesso.innerHTML = MensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
-        valorA.value = '';
-        valorB.value = '';
+        document.getElementById('info-a').value = '';
+        document.getElementById('info-b').value = '';
+        document.querySelector('.message-erro').style.display = 'none'
     }
     else{
     const containerMensagemerro = document.querySelector('.message-erro')
     containerMensagemerro.innerHTML = MensagemErro;
     containerMensagemerro.style.display = 'block';
+    
     }
 
 })
